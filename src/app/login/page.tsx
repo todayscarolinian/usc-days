c70a7b1728483b839138ad3b83d82979f95ab961
wiswiss <luisandreiouano@gmail.com>
2024-09-18 23:32:30 +0800

    feat: create login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from '@/app/login/login-form'
+
+export default async function Page() {
+  return (
+    <div className='flex items-center justify-center min-h-screen'>
+      <LoginForm />
+    </div>
+  )
+}
