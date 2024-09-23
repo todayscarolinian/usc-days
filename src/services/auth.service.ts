402502f42557cce1a50e671554941fff300e1471
Zach Riane Machacon <zachriane01@gmail.com>
2024-09-23 23:53:42 +0800

    feat: added libs for supabase and prisma, and route for auth

---
+++ src/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { prisma } from '@/lib/prisma'; // Ensure correct path to Prisma client
+import { Prisma } from '@prisma/client';
+
+class AuthService {
+
+    static async isUserInDatabase({ email }: { email: string }) {
+        try {
+            // Check if the user's email exists in the `users` table using Prisma
+            const user = await prisma.user.findUnique({
+                where: { email },
+            });
+
+            if (!user) {
+                return false;
+            }
+
+            return true;
+        } catch (error) {
+            if (error instanceof Prisma.PrismaClientKnownRequestError) {
+                throw new Error('Database request failed');
+            }
+
+            throw new Error('An unexpected error occurred while fetching the user');
+        }
+    }
+}
+
+export default AuthService;
+
