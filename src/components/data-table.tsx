58aa72a93f9b2d9ae155d4772f4bfadc51dcd6a2
John Rey Millor <johnreymillor69@gmail.com>
2024-09-24 09:59:15 +0800

    making the filters button color red for UI stability

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -92,7 +92,7 @@ export function DataTable<TData, TValue>({
           />
           <button
             onClick={handleOpenAdvancedSearch}
-            className="bg-blue-500 text-white px-4 py-2 rounded"
+            className="bg-red-600 text-white px-4 py-2 rounded"
           >
             Filters
           </button>
