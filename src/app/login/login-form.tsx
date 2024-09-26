41b0f14f2ee5d67731205281604516efdb77f964
wiswiss <laeouano@gmail.com>
2024-09-26 23:06:59 +0800

    feat: set error response description for invalid login

--- src/app/login/login-form.tsx
+++ src/app/login/login-form.tsx
@@ -25,7 +25,7 @@ export function LoginForm() {
       toast({
         variant: "destructive",
         title: "Login Failed!",
-        description: "Invalid username or password",
+        description: response?.error,
       })
     }
   }
