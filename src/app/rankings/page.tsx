83282ac9f4b94be9057bb68775fcca1696fdef7a
Seaside <n.peteros2003@gmail.com>
2024-10-20 04:02:20 +0800

    fixed deploy errors

--- src/app/rankings/page.tsx
+++ src/app/rankings/page.tsx
@@ -1,18 +1,19 @@
-"use client"
+"use client";
 
 import { useState } from "react";
-import { TeamRankingsTable } from "./team-rankings-table";
+// import { TeamRankingsTable } from "./team-rankings-table";
 import { SelectSportButton } from "./select-sport";
 
 export default function RankingSummaryPage() {
-  const [selectedSport, setSelectedSport] = useState<string | null>(null);
+    const [selectedSport, setSelectedSport] = useState<string | null>(null);
+    console.log(selectedSport);
 
-  return (
-    <div className='p-11'>
-      <div className="mb-11">
-        <SelectSportButton onSelectSport={setSelectedSport} />
-      </div>
-      <TeamRankingsTable selectedSport={selectedSport} />
-    </div>
-  );
+    return (
+        <div className="p-11">
+            <div className="mb-11">
+                <SelectSportButton onSelectSport={setSelectedSport} />
+            </div>
+            {/* <TeamRankingsTable selectedSport={selectedSport} /> */}
+        </div>
+    );
 }
