c6864ed7d275cd9ef70bd0483789d048e4b4d4c3
Seaside <n.peteros2003@gmail.com>
2024-10-19 12:18:28 +0800

    implemented GET request for score page

--- src/components/Scores/scores.tsx
+++ src/components/Scores/scores.tsx
@@ -1,14 +1,16 @@
 import { scoreColumns } from "@/components/scores/columns"
 import { DataTable } from "@/components/scores/score-table"
+import axios from "axios"
 
-// Currently static data. To be Fetched from the db using the api
-import { mockScoresData } from "@/constants/mockData"
+export default async function ScoresPage() {
+    const { data: gamesData } = await axios.get(process.env.NEXT_PUBLIC_BASE_URL + "/api/games");
+    console.log(gamesData.games[0].gameType);
+    
 
-export default function Scores() {
   return (
     <div className="p-4 sm:py-10">
       <div className="mx-auto sm:max-w-[90rem]">
-        <DataTable columns={scoreColumns} data={mockScoresData} />
+        <DataTable columns={scoreColumns} data={gamesData.games} />
       </div>
     </div>
   )
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,31 +2,38 @@
   Types for the score, Initialy set to string for all types for testing purposes. Will be changed later to match DB types
 */
 export type Scores = {
-  id: number,
-  date: string,
-  sport: string,
-  teams: {
-    home: string,
-    away: string,
-  },
-  scores: {
-    home: number,
-    away: number,
-  },
-  winner: string,
+    id: number,
+    startDate: string,
+    gameType: {
+        id: number,
+        gameName: string,
+    },
+    teamA: {
+        id: number,
+        teamName: string,
+    }
+    teamB: {
+        id: number,
+        teamName: string,
+    }
+    score: {
+        teamAScore: number,
+        teamBScore: number,
+    },
+    winner: string,
 }
 
 export type Champions = {
-  id: string,
-  sport: string,
-  team: string,
+    id: string,
+    sport: string,
+    team: string,
 }
 
 export type filterType = {
-  date?: string,
-  game?: string,
-  teams?: {
-    home?: string,
-    away?: string,
-  }
+    date?: string,
+    game?: string,
+    teams?: {
+        home?: string,
+        away?: string,
+    }
 };
