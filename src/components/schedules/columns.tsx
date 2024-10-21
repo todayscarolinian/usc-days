4befda28be421928811ae37a35cd167329e14d9b
Seaside <n.peteros2003@gmail.com>
2024-10-21 21:59:15 +0800

    added new icons

--- src/components/schedules/columns.tsx
+++ src/components/schedules/columns.tsx
@@ -3,28 +3,38 @@
 import { ColumnDef, SortingFn } from "@tanstack/react-table";
 import { Schedules } from "@/types/types";
 import { format } from "date-fns";
+
 import Badminton from "@/assets/icons/Diamond/Badminton.svg";
 import Basketball from "@/assets/icons/Diamond/Basketball.svg";
+import Cheerdance from "@/assets/icons/Diamond/Cheerdance.svg";
 import Chess from "@/assets/icons/Diamond/Chess.svg";
 import Esports from "@/assets/icons/Diamond/Esports.svg";
 import FlagFootball from "@/assets/icons/Diamond/Flag Football.svg";
 import Football from "@/assets/icons/Diamond/Football.svg";
+import Frisbee from "@/assets/icons/Diamond/Frisbee.svg";
 import Futsal from "@/assets/icons/Diamond/Futsal.svg";
 import LawnTennis from "@/assets/icons/Diamond/Lawn Tennis.svg";
+import MrIntrams from "@/assets/icons/Diamond/Mr Intrams.svg";
+import MsIntrams from "@/assets/icons/Diamond/Ms Intrams.svg";
+import Swimming from "@/assets/icons/Diamond/Swimming.svg";
 import TableTennis from "@/assets/icons/Diamond/Table Tennis.svg";
+import ThreeByThreeBasketball from "@/assets/icons/Diamond/ThreeByThreeBasketball.svg";
 import Volleyball from "@/assets/icons/Diamond/Volleyball.svg";
 import Image from "next/image";
 
 const sportIcons: Record<string, JSX.Element> = {
-    Badminton: (
+    "Badminton": (
         <Image src={Badminton} className="inline mr-2 size-6" alt="Badminton" />
     ),
-    Basketball: (
+    "Basketball": (
         <Image
             src={Basketball}
             className="inline mr-2 size-6"
             alt="Basketball"
         />
+    ),
+    "Cheer Dance": (
+        <Image src={Cheerdance} className="inline mr-2 size-6" alt="Cheer Dance" />
     ),
     Chess: <Image src={Chess} className="inline mr-2 size-6" alt="Chess" />,
     "E-Sports": (
@@ -37,12 +47,24 @@ const sportIcons: Record<string, JSX.Element> = {
             alt="Flag Football"
         />
     ),
-    Football: (
+    "Football": (
         <Image src={Football} className="inline mr-2 size-6" alt="Football" />
     ),
-    Futsal: <Image src={Futsal} className="inline mr-2 size-6" alt="Futsal" />,
+    "Frisbee": (
+        <Image src={Frisbee} className="inline mr-2 size-6" alt="Frisbee" />
+    ),
+    "Futsal": <Image src={Futsal} className="inline mr-2 size-6" alt="Futsal" />,
     "Lawn Tennis": (
-        <Image src={LawnTennis} className="inline mr-2 size-6" alt="Futsal" />
+        <Image src={LawnTennis} className="inline mr-2 size-6" alt="Lawn Tennis" />
+    ),
+    "Mr. USC Days": (
+        <Image src={MrIntrams} className="inline mr-2 size-6" alt="Mr. USC Days" />
+    ),
+    "Ms. USC Days": (
+        <Image src={MsIntrams} className="inline mr-2 size-6" alt="Ms. USC Days" />
+    ),
+    "Swimming": (
+        <Image src={Swimming} className="inline mr-2 size-6" alt="Swimming" />
     ),
     "Table Tennis": (
         <Image
@@ -51,7 +73,10 @@ const sportIcons: Record<string, JSX.Element> = {
             alt="Table Tennis"
         />
     ),
-    Volleyball: (
+    "3x3 Basketball": (
+        <Image src={ThreeByThreeBasketball} className="inline mr-2 size-6" alt={"3x3 Basketball"} />
+    ),
+    "Volleyball": (
         <Image
             src={Volleyball}
             className="inline mr-2 size-6"
