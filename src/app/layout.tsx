53ca43082e22faca077376c5798623e0ae191e3e
joms-hub <21101932@usc.edu.ph>
2025-09-27 17:50:19 +0800

    fix: change img property to tc-logo-red

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
         title: "USC Days 2025 Scoreboard - Today's Carolinian",
         description: 'Scoreboard of the different school teams per game for USC Days 2025',
         url: '/',
-        image: '/tc-logo-white.png'
+        image: '/tc-logo-red.png'
     }),
     keywords: ['USC Days', 'scoreboard', 'sports', 'University of San Carlos', 'intramurals', 'competition'],
     authors: [{ name: "Today's Carolinian" }],
