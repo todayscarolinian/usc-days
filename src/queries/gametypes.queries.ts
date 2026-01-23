95bd599a6ba67bde9c6af68fd6eff62b387e8b92
alessaaandraaa <24100314@usc.edu.ph>
2026-01-23 17:21:25 +0800

    Feat: added merchandise API routes and queries

--- src/queries/gametypes.queries.ts
+++ src/queries/gametypes.queries.ts
@@ -2,18 +2,18 @@ import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
 type GameType = {
-    id: number;
-    gameName: string;
+  id: number;
+  gameName: string;
 };
 
 const STALE_TIME = 1000 * 60 * 5;
 
 export const getGameTypesQuery = () =>
-    useQuery<GameType[]>({
-        queryKey: ["sports"],
-        queryFn: async () => {
-            const response = await axios.get("/api/sports");
-            return response.data.sports;
-        },
-        staleTime: STALE_TIME,
-    });
+  useQuery<GameType[]>({
+    queryKey: ["sports"],
+    queryFn: async () => {
+      const response = await axios.get("/api/sports");
+      return response.data.sports;
+    },
+    staleTime: STALE_TIME,
+  });
