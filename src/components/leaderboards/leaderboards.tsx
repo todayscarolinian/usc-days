b227222134af62c2a25c436f746d1f25688136b8
Neal Peteros <n.peteros2003@gmail.com>
2026-01-27 14:44:17 +0800

    chore: updated pages to use the shared SportSelector compopnent

--- src/components/leaderboards/leaderboards.tsx
+++ src/components/leaderboards/leaderboards.tsx
@@ -6,7 +6,7 @@ import { DataTable } from "@/src/components/leaderboards/data-table";
 import { columns } from "@/src/components/leaderboards/columns";
 import { transformGamesToSchoolRank } from "@/src/components/leaderboards/transformData";
 import { getGamesQuery } from "@/src/queries/games.queries";
-import SportSelector from "@/src/components/leaderboards/sport-selector";
+import SportSelector from "@/src/components/ui/sport-selector";
 import LeaderboardsTableSkeleton from "@/src/components/leaderboards/leaderboards-table-skeleton";
 import { StandingData } from "@/src/types/types";
 
@@ -43,7 +43,11 @@ export default function Leaderboards() {
   return (
     <div className="p-4 sm:py-10">
       <div className="mx-auto max-w-[96%] space-y-6">
-        <SportSelector selected={selectedSport} onSelect={handleSportSelect} />
+        <SportSelector
+          selected={selectedSport}
+          onSelect={handleSportSelect}
+          triggerClassName="flex items-center justify-between !px-[22px] !py-[7px] !h-[54px] min-w-full bg-white shadow-sm rounded-[2px] border border-neutral-200 border-l-[2px] transition-colors hover:border-l-tc_primary-500 data-[state=open]:border-l-tc_primary-500 outline-none [&>svg.size-4.opacity-50]:hidden"
+        />
         {error || loading ? (
           <LeaderboardsTableSkeleton error={error?.message} />
         ) : (
