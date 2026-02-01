12ab8cfe33a381303627f757ba36d790ea64dbe7
cl1ffhan8er <24101922@usc.edu.ph>
2026-02-01 17:00:31 +0800

    feat: "change UI for Schedule Date"

--- src/components/schedules/add-schedule-dialog.tsx
+++ src/components/schedules/add-schedule-dialog.tsx
@@ -154,7 +154,7 @@ export default function AddScheduleDialog() {
           <div className="w-full grid grid-cols-1 gap-4">
             <div className="flex flex-col gap-1">
               <Label htmlFor="startDate" className="font-bold opacity-50">
-                Start Date
+                Date
               </Label>
               <Input
                 type="date"
