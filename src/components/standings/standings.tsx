7ef04e5b9eada862dd5cd87411048b5bfc01c2b6
Thunderquacker <matthewmayol28@gmail.com>
2026-02-06 14:00:24 +0800

    Revised remove add standings

--- src/components/standings/standings.tsx
+++ src/components/standings/standings.tsx
@@ -248,19 +248,7 @@ export default function Standings() {
     [championsData, selectedSport],
   );
 
-  const handleAddStanding = useCallback(() => {
-    if (!selectedSport) return;
-
-    setFormData({
-      id: -1,
-      gameTypeId: selectedSport,
-      rank: 1, // Default to first place
-      teamId: -1,
-      endDate: new Date().toISOString().split("T")[0],
-      startDate: new Date().toISOString().split("T")[0],
-    });
-    setShowDialog(true);
-  }, [selectedSport]);
+  
 
   const handleCloseDialog = useCallback(() => {
     setFormData(null);
@@ -286,15 +274,7 @@ export default function Standings() {
             onValueChangeAction={handleSportSelect}
             className="flex items-center justify-between px-5.5! py-1.75! h-13.5! max-w-xs bg-white shadow-sm rounded-[2px] border border-neutral-200 border-l-2 transition-colors hover:border-l-tc_primary-500 data-[state=open]:border-l-tc_primary-500 outline-none [&>svg.size-4.opacity-50]:hidden"
           />
-          {isAdmin && (
-            <Button
-              variant="default"
-              disabled={!selectedSport}
-              onClick={handleAddStanding}
-            >
-              + Add Standing
-            </Button>
-          )}
+          
         </div>
 
         {loading && selectedSport && (
