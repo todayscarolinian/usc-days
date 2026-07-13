5a38fef4d7646e9f45419646625846a4e7c25c63
Neal Peteros <n.peteros2003@gmail.com>
2026-07-13 18:18:45 +0800

    chore: remove unnecessary comments

--- src/lib/herald/use-has-domain-access.ts
+++ src/lib/herald/use-has-domain-access.ts
@@ -4,10 +4,8 @@ import { REQUIRED_DOMAIN, type HeraldUser } from "./types";
 
 export function useHasHeraldDomainAccess() {
   const { data: session, isPending } = useSession();
-  console.log("session", session);
   const user = session?.user as HeraldUser | undefined;
   const domains = user?.domains ?? [];
-  console.log("user", user);
 
   return {
     isPending,
